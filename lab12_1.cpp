15802a3a1b09cc55740a64b4601d2643006c3fe4
SkyEngineerME <guymechatronic@gmail.com>
2023-01-06 20:53:21 +0700

    lab12Done

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+string text;
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()%9;
+    if(x == 0) text = "A" ;
+    else if(x == 1) text = "B+";
+    else if(x == 2) text = "B";
+    else if(x == 3) text = "C+";
+    else if(x == 4) text = "C";
+    else if(x == 5) text = "D+";
+    else if(x == 6) text = "D";
+    else if(x == 7) text = "F";
+    else if(x == 8) text = "W";
+
+    cout << "You will get "<< text << " in this 261102.";
+    return 0;
+}
+
